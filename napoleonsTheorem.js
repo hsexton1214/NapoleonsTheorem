9b7683c72aec56f7cd3f6ebc33c996835a8aff8b
nleatherman <leathermannatalie@gmail.com>
2016-04-15 17:14:43 -0400

    drawCircle

--- napoleonsTheorem.js
+++ napoleonsTheorem.js
@@ -21,8 +21,16 @@ function drawMidpoint(x1,y1,x2,y2){
 };//drawMidpoint
 function drawCenteroids(x1,y1,x2,y2,x3,y3){
 };
-function drawCircle(x1,y1,x2,y2){  
-}//drawCircle
+function drawCircle(x1,y1,x2,y2){ 
+    var circleVertices = [];
+    var inc = 2*Math.PI / 50;
+    var radius = Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+    
+    for (var theta = 0; theta <2*Math.PI; theta += inc){
+        circleVertices.push(vec2(radius*Math.cos(theta)+x1, radius*Math.sin(theta)+y1));
+    }
+    return circleVertices;
+};//drawCircle
 function drawLine(x1,y1,x2,y2){
     
-}//drawLine
+};//drawLine
