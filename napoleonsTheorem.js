5fcf43151e32e4160312ed0f404299d5c057301c
hsexton1214 <dancer1214@gmail.com>
2016-04-15 14:18:31 -0400

    js file and functions

---
+++ napoleonsTheorem.js
@@ -0,0 +1,14 @@
+/* 
+ * Hannah Sexton
+ * Natalie Leatherman
+ * 
+ */
+function drawTriangle(x1,y1,x2,y2,x3,y3){  
+};//drawTriangle
+function drawMidpoint(x1,y1,x2,y2){
+};//drawMidpoint
+function drawCenteroids(x1,y1,x2,y2,x3,y3){
+};
+
+
+
