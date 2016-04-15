a9c0c81af3257b766b28c7c66598b878c463d057
nleatherman <leathermannatalie@gmail.com>
2016-04-15 16:02:48 -0400

    canvasMain, drawCircle, and drawLine added

--- napoleonsTheorem.js
+++ napoleonsTheorem.js
@@ -3,12 +3,26 @@
  * Natalie Leatherman
  * 
  */
+
+var gl;
+var points;
+
+function canvasMain() {
+    var canvas = document.getElementById("gl-canvas");
+    gl = WebGLUtils.setupWebGL(canvas);
+    if (!gl) {
+        alert("WebGL isn't available");
+    }
+    
+}
 function drawTriangle(x1,y1,x2,y2,x3,y3){  
 };//drawTriangle
 function drawMidpoint(x1,y1,x2,y2){
 };//drawMidpoint
 function drawCenteroids(x1,y1,x2,y2,x3,y3){
 };
-
-
-
+function drawCircle(x1,y1,x2,y2){  
+}//drawCircle
+function drawLine(x1,y1,x2,y2){
+    
+}//drawLine
