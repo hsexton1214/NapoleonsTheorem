011bca7d346879f64c8c0c73bc91073ff31ecb19
hsexton1214 <dancer1214@gmail.com>
2016-04-15 17:14:32 -0400

    draw object

--- napoleonsTheorem.js
+++ napoleonsTheorem.js
@@ -13,9 +13,19 @@ function canvasMain() {
     if (!gl) {
         alert("WebGL isn't available");
     }
+    gl.viewport(0,0, canvas.width, canvas.height);
+    gl.clearColor(0.0,0.0,0.0,1.0);
     
-}
-function drawTriangle(x1,y1,x2,y2,x3,y3){  
+    var program = initShaders(gl,"vertex-shader", "fragment-shader");
+    gl.useProgram(program);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    
+    ///draw object
+    var objColor =[.5,.5,.5,1];
+    
+};//canvas main
+
+function drawTriangle(x1,y1,x2,y2,x3,y3){
 };//drawTriangle
 function drawMidpoint(x1,y1,x2,y2){
 };//drawMidpoint
@@ -25,4 +35,20 @@ function drawCircle(x1,y1,x2,y2){
 }//drawCircle
 function drawLine(x1,y1,x2,y2){
     
-}//drawLine
+};//drawLine
+function drawPoint(x,y){};//drawPoint
+
+function drawObject(gl, program, vertices, color, glType){
+    var colorLocation = gl.getUniformLocation(program,"uColor");
+    
+    var bufferId = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW);
+    
+    var vPosition = gl.getAttribLocation(program, "vPosition");
+    gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vPosition);
+    
+    gl.uniform4f(colorLocation, color[0], color[1], color[2], color[3]);
+    gl.drawArrays(glType, 0, vertices.length);
+}//drawObject
